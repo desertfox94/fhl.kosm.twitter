9bf60ddd17a458a2f11eca854972bf53b6a1e821
Lea <lea@fritzzone.de>
2018-06-05 13:07:08 +0200

    SpringBoot eingebunden

--- BubbleBuster/src/main/java/fhl/kosm/twitter/App.java
+++ BubbleBuster/src/main/java/fhl/kosm/twitter/App.java
@@ -1,11 +1,19 @@
 package fhl.kosm.twitter;
 
 import fhl.kosm.twitter.analyse.HashtagAnalyser;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RestController;
 
+
+@EnableAutoConfiguration
+@ComponentScan()
 public class App {
 
 	public static void main(String[] args) throws Exception {
-		new HashtagAnalyser().relations("ElonMusk", "Tesla", "SpaceX", "Model3", "flaconheavy", "KI");
+		//new HashtagAnalyser().relations("ElonMusk", "Tesla", "SpaceX", "Model3", "flaconheavy", "KI");
+		SpringApplication.run(App.class, args);
 	}
 
 }
